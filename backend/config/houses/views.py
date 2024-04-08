0337587a1b98d36157a430abacb055198adcde4b
Arturo Parrales <arturo.ps14@gmail.com>
2024-04-07 22:56:49 -0400

    Added viw actions for house model

--- backend/config/houses/views.py
+++ backend/config/houses/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
 
-# Create your views here.
+from rest_framework.response import Response
+
+from .models import Checker
+from .models import House
+from .serializers import HouseSerializer
+
+class HouseViewSet(viewsets.ViewSet):
+    def list(self, request):
+        houses = House.objects.all()
+        houses_serializer = HouseSerializer(houses, many=True)
+        return Response(houses_serializer.data)
+
+    def create(self, request):
+        house_serializer = HouseSerializer(data=request.data)
+        house_serializer.is_valid(raise_exception=True)
+        house_serializer.save()
+        return Response(house_serializer.data, status=status.HTTP_201_CREATED)
+    
+    
