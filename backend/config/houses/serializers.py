2488d34f16119dc3655d788d86df67f2d848b582
Arturo Parrales <arturo.ps14@gmail.com>
2024-05-02 16:16:48 +0900

    Fixed typos

--- backend/config/houses/serializers.py
+++ backend/config/houses/serializers.py
@@ -4,4 +4,4 @@ from .models import House
 
 class HouseSerializer(serializers.ModelSerializer):
     model = House
-    fields - "__all__"
+    fields = "__all__"
--- backend/config/houses/views.py
+++ backend/config/houses/views.py
@@ -2,8 +2,8 @@ import random
 
 from rest_framework import viewsets
 from rest_framework import status
-from rest_framework import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Checker
 from .models import House
